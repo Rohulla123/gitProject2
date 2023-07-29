8f774fc5f597137c519afab254c93bdc7335301f
Rohulla123 <rohullah.asify@gmail.com>
2023-07-29 12:18:32 -0400

    initiated

---
+++ src/Day3/DemoDay3.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class DemoDay3 {
+	public static void main(String[] args) {
+		
+	
+
+	String country = "USA";
+	String state = "VA";
+	
+	
+	
+	}
+	
+}
--- src/GitProject1/DemoDay2.java
+++ src/GitProject1/DemoDay2.java
@@ -1,5 +1,31 @@
 package GitProject1;
 
 public class DemoDay2 {
+	public static void main(String[] args) {
+		System.out.println("Test");
+		
+		System.out.println("Test2");
+	}
+
+		// add method
+		public static void add(int a, int b) {
+
+		}
+		public static void add(int a, int b, int c) {
+
+			
+			System.out.println(a+b +c);
+		}
+		
+		// subtract
+		public static void subtract( int a, int b) {
+			System.out.println(a-b);
+		}
+		// Yasir
+				public static void subtract1( int a, int b) {
+					System.out.println(a-b);
+	
+}
+				
 
 }
